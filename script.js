29674bc7bf9c57a0df00d7de223dc905cb85ee47
scicluna <88948700+scicluna@users.noreply.github.com>
2022-11-18 16:42:36 -0500

    We did it. the dice roller is amazing

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ const string = document.querySelector(".dice-string")
 const resultsList = document.querySelector(".results")
 const totalsList = document.querySelector(".totals")
 const rollBtn = document.querySelector(".roller")
+explosiveFlag = "off"
 
 //splitting the string into arrays of an array
 function splitString (str) {
     let splitStrings = []
-    let subArray = str.split(/[-+]/)
+    let subArray = str.split(/[+]/)
 
     while(subArray.length){
     subString = subArray.splice(0,1);
     splitStrings.push(subString)
     }
-    
     return splitStrings
 }
 
@@ -27,6 +27,7 @@ function splitArray (array) {
 return cuttedArrays
 }
 
+
 //roll a single dice
 function diceRoll(size){
     let rolledDie = Math.ceil(Math.random()*size)
@@ -34,17 +35,38 @@ function diceRoll(size){
 }
 
 //roll many dice
-function allRoll(quantity, size){
+function allRoll(quantity, size, drop){
     let rolledDice = []
+
+//handling explosive dice syntax
+    if (size != undefined && size.includes('!')){
+        size = size.replace(/!/g, '')
+        explosiveFlag = "on"
+    }
+//running a loop to roll the dice per quantity
     if (size != undefined){
     for (i=0; i<quantity; i++){
-        rolledDice.push(diceRoll(size))
+        const addDice =diceRoll(size)
+    //handling explosive dice rolling
+        if (addDice == size && explosiveFlag == "on"){
+            i--
+            explosiveFlag = "off"
+        }
+        rolledDice.push(addDice)
+//handling regular addition
     }}
     else {
         let addition = parseInt(quantity)
         rolledDice.push(addition);
     }
 
+    if (drop != undefined){
+        rolledDice = rolledDice.sort((a,b) => b-a).slice(0,rolledDice.length-drop)
+    }
+
+    else {
+        rolledDice = rolledDice.sort((a,b) => b-a)
+    }
     return rolledDice
 }
 
@@ -52,9 +74,8 @@ function allRoll(quantity, size){
 function rollAll(array){
     allDice = []
     for (let i=0; i<array.length;i++){
-        allDice.push(allRoll(array[i][0],array[i][1]));
+        allDice.push(allRoll(array[i][0],array[i][1],array[i][2]));
     }
-    console.log(allDice)
     return allDice
 }
 
@@ -66,14 +87,14 @@ function totalAll(array){
             total = total + array[i][j]
         }
     }
-    console.log(total)
     return total
 }
 
 rollBtn.addEventListener('click', function(){
     //init functions
-    rollAll(splitArray(splitString(string.value)));
-    totalAll(rollAll(splitArray(splitString(string.value))));
+    const arraySplit = splitArray(splitString(string.value))
+    const rolledAll = rollAll(arraySplit);
+    totalAll(rolledAll);
     //Create an Li
     const newLi = document.createElement('LI');
     //liContent must equal the content I want to display 
@@ -81,9 +102,17 @@ rollBtn.addEventListener('click', function(){
     newLi.appendChild(liContent);
     //Attach the Li to the results list
     resultsList.appendChild(newLi);
+        //cool animation
+        setTimeout(function(){
+            newLi.className = newLi.className +" show"; 
+        }, 10);
     //Repeat for Total 
     const newLi2 = document.createElement("LI");
     const liContent2 = document.createTextNode(total);
     newLi2.appendChild(liContent2);
     totalsList.appendChild(newLi2);
+            //cool animation
+            setTimeout(function(){
+                newLi2.className = newLi2.className +" show"; 
+            }, 10);
 })
